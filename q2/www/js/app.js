2c95ed0fe6a567d64e875ec104d28733f15d1c56
wlu-rtram <tram0290@mylaurier.ca>
2019-11-04 22:54:11 -0500

    added form validation

---
+++ q2/www/js/app.js
@@ -0,0 +1,111 @@
+function validateEmail() {
+  var email = document.getElementById("email").value;
+
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var test = re.test(String(email).toLowerCase());
+
+  if (!test) {
+    alert('Please enter a valid email address.')
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateEmailEmpty() {
+  var email = document.getElementById("email").value;
+
+  if (email == "") {
+    alert("Empty email.");
+    document.getElementById("email").focus();
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateUsername() {
+  var username = document.getElementById("username").value;
+
+  var re = /^[a-zA-Z]+([_-]?[a-zA-Z0-9])*$/;
+  var test = re.test(String(username));
+
+  if (!test) {
+    alert('Please enter a valid username.')
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateUsernameEmpty() {
+  var email = document.getElementById("username").value;
+
+  if (email == "") {
+    alert("Empty username.");
+    document.getElementById("username").focus();
+
+    return false;
+  }
+
+  return true;
+}
+
+function validatePassword() {
+  var password = document.getElementById("password").value;
+
+  var re = /^[a-zA-Z]+([_-]?[a-zA-Z0-9])*$/;
+  var test = re.test(String(password));
+
+  if (!test) {
+    alert('Please enter a valid password.')
+
+    return false;
+  }
+
+  return true;
+}
+
+function validatePasswordEmpty() {
+  var email = document.getElementById("password").value;
+
+  if (email == "") {
+    alert("Empty password.");
+    document.getElementById("password").focus();
+
+    return false;
+  }
+
+  return true;
+}
+
+function validateForm() {
+  if (!validateEmailEmpty()) {
+    return false;
+  }
+
+  if (!validateEmail()) {
+    return false;
+  }
+
+  if (!validateUsernameEmpty()) {
+    return false;
+  }
+
+  if (!validateUsername()) {
+    return false;
+  }
+
+  if (!validatePasswordEmpty()) {
+    return false;
+  }
+
+  if (!validatePassword()) {
+    return false;
+  }
+
+  return true;
+}
